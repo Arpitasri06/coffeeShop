b5e5e7e6fcd5de0c043de72e326a2bf7363de45c
Arpita Srivastava <ar.srivastava@mum.edu>
2018-06-10 17:36:22 -0500

    Remaining Controllers and User Interface

--- src/main/java/edu/mum/coffee/controller/HomeController.java
+++ src/main/java/edu/mum/coffee/controller/HomeController.java
@@ -1,18 +1,38 @@
 package edu.mum.coffee.controller;
 
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import edu.mum.coffee.domain.Product;
+import edu.mum.coffee.service.ProductService;
 
 @Controller
 public class HomeController {
-	
-	@GetMapping({"/", "/index", "/home"})
-	public String homePage() {
-		return "login";
+
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping({ "/", "/index", "/home" })
+	public String homePage(Model model) {
+		populateModel(model);
+		return "home/index";
 	}
 
-	@GetMapping({"/secure"})
+	private void populateModel(Model model) {
+
+		
+		List<Product> products = productService.getAll();
+		model.addAttribute("products", products);
+
+	}
+
+	@GetMapping({ "/secure" })
 	public String securePage() {
-		return "secure";
+		return "home/secure";
 	}
 }
